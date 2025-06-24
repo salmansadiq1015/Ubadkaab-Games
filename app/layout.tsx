444696480ea6194b5cd037131207a4a990af94e0
Salman Sadiq <118099469+salmansadiq1015@users.noreply.github.com>
2025-06-24 11:10:54 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Ubadkaab Games',
+  description: 'Created by Salman Sadiq',
 }
 
 export default function RootLayout({
